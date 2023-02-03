159b3e001741ca3486f68470a5dfe89bd8ca2cfc
Arman Grema <abdurrahmangrema19@gmail.com>
2023-02-02 20:18:46 -1100

    defined variables

---
+++ script.js
@@ -0,0 +1,5 @@
+// defining the variables
+var batteryFrame = document.querySelector('.shell')
+var height = document.querySelector('.parcentage-height');
+var level = document.querySelector('.parcentage-digit');
+var statu = document.querySelector('.status')
