75af0d2ce664667823a477330ed9cab210534472
Arman Grema <abdurrahmangrema19@gmail.com>
2023-02-02 20:25:38 -1100

    configured the battery-api listening charging status

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ var batteryFrame = document.querySelector('.shell')
 var height = document.querySelector('.parcentage-height');
 var level = document.querySelector('.parcentage-digit');
 var statu = document.querySelector('.status')
+
+// config battery api
+
+navigator.getBattery().then((battery) => {
+    function updateAllBatteryInfo() {
+      updateChargeInfo();
+      updateLevelInfo();
+    }
+    updateAllBatteryInfo();
+  // watch charging plugin status
+    battery.addEventListener("chargingchange", () => {
+      updateChargeInfo();
+    });
+    function updateChargeInfo() {
+       console.log(battery.charging)
+       if(battery.charging){
+         statu.innerHTML = "charging"
+       }else{
+        statu.innerHTML = " "
+       }
+    }
+  
+  
+    
+  
+  });
